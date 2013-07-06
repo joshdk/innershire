a176ad6d2a709eb96e82cf51bc4612a0a9ae4b5a
Josh Komoroske <jdkomo@gmail.com>
2013-07-06 01:13:51 -0700

    Refactored most of the code to better utilize jQuery

--- assets/custom/js/main.js
+++ assets/custom/js/main.js
@@ -1,5 +1,13 @@
 ;(function(geoPosition, $, shire){
 
+	var $address = false;
+	var $answerInner = false;
+	var $answerOuter = false;
+	var $formLocate = false;
+	var $formSubmit = false;
+	var $form = false;
+
+
 	var fillLocation = function(){
 		if(!geoPosition.init()){
 			alert("Error: Your device does not currently support Geolocation.");
@@ -8,7 +16,7 @@
 
 		geoPosition.getCurrentPosition(
 			function(position){
-				document.getElementById("form-address").value = position.coords.latitude + ", " + position.coords.longitude;
+				$address.val(position.coords.latitude + ", " + position.coords.longitude);
 			},
 			function(){
 				alert("Error: Could not obtain device Geolocation.");
@@ -16,70 +24,79 @@
 		);
 	}
 
+
 	var searchLocation = function(){
 
-		var address = document.getElementById("form-address").value;
-		var answerNode = document.getElementById("answer");
+		shire.setLocation($address.val());
 
-		shire.init($.getJSON);
-		shire.setLocation(address);
+		$answerOuter.slideUp(500, function(){
+			$answerInner.slideUp(500).text("Searching the Inner Shire...").slideDown(500, function(){
 
-		answerNode.innerHTML = "Searching...";
-
-		shire.checkInnerShire(
-			function(isInner, time){
-
-				if(isInner){
-					console.log("You are in the inner shire!");
-					answerNode.innerHTML = "You are in the inner shire!";
-					return;
-				}
-//{{{ Check outer shire
-				shire.checkOuterShire(
-					function(isOuter, time){
-						console.log(isOuter);
-						console.log(time);
-
-						if(isOuter){
-							console.log("You are in the outer shire!");
-							answerNode.innerHTML = "You are in the outer shire!";
-							return;
+				shire.checkInnerShire(
+					function(isInner, time){
+						if(isInner){
+							console.log("You are in the inner shire!");
+							$answerInner.text("You are in the inner shire!");
+						}else{
+							$answerInner.text("You are not in the inner shire...");
 						}
 
-						console.log("You are outside the shire...");
-						answerNode.innerHTML = "You are outside the shire entirely...";
+						$answerOuter.slideDown(500).text("Searching the Outer Shire...");
+
+						shire.checkOuterShire(
+							function(isOuter, time){
+								if(isOuter){
+									console.log("You are in the Outer Shire!");
+									$answerOuter.text("You are in the Outer Shire!");
+								}else{
+									$answerOuter.text("You are not in the Outer Shire...");
+								}
+							},
+							function(){
+								console.log("checkOuterShire failed...");
+								$answerOuter.slideUp(500);
+								alert("Error: Failed to query Google Maps for the given location.");
+							}
+						);
 
 					},
 					function(){
-						console.log("checkOuterShire failed...");
+						console.log("checkInnerShire failed...");
+						$answerInner.slideUp(500);
+						alert("Error: Failed to query Google Maps for the given location.");
 					}
 				);
-//}}}
-			},
-			function(){
-				console.log("checkInnerShire failed...");
-			}
-		);
+
+			});
+		});
 
 	}
 
 
-
 	window.onload = function(){
-		document.getElementById("form-locate").onclick = function(){
+
+		$address = $("#form-address");
+		$answerInner = $("#answer-inner");
+		$answerOuter = $("#answer-outer");
+		$formLocate = $("#form-locate");
+		$formSubmit = $("#form-submit");
+		$form = $("#form");
+		shire.init($.getJSON);
+
+		$formLocate.click(function(){
 			fillLocation();
 			return false;
-		}
+		});
 
-		document.getElementById("form-submit").onclick = function(){
+		$formSubmit.click(function(){
 			searchLocation();
 			return false;
-		}
+		});
 
-		document.getElementById("form").onsubmit = function(){
+		$form.submit(function(){
 			searchLocation();
 			return false;
-		}
+		});
 
 	};
 
